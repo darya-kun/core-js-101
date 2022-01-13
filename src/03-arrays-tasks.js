43d07404a80d2b89ef0b943d38708376a127c38e
Darya <darya.drazdouskaya@gmail.com>
2022-01-13 18:58:42 +0300

    feat: add toArrayOfSquares solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -217,9 +217,10 @@ function toCsvText(arr) {
  *   [ 0, 1, 2, 3, 4, 5 ] => [ 0, 1, 4, 9, 16, 25 ]
  *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
  */
-function toArrayOfSquares(/* arr */) {
-  throw new Error('Not implemented');
+function toArrayOfSquares(arr) {
+  return arr.map((element) => element * element);
 }
+console.log(toArrayOfSquares([0, 1, 2, 3, 4, 5]));
 
 
 /**

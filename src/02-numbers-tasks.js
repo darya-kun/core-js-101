a5579ad96cadfd25e2b2539323769ce312e97eb2
Darya <darya.drazdouskaya@gmail.com>
2022-01-13 15:12:15 +0300

    feat: add getLastDigit solution

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -126,8 +126,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  // return value.toString().slice(-1);
+  return String(value).slice(-1);
 }
 
 

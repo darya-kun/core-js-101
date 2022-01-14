405ec2d76631e1327e2dab22aef44d89c9c2093c
Darya <darya.drazdouskaya@gmail.com>
2022-01-14 14:01:07 +0300

    feat: add getMovingSum solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -220,7 +220,6 @@ function toCsvText(arr) {
 function toArrayOfSquares(arr) {
   return arr.map((element) => element * element);
 }
-console.log(toArrayOfSquares([0, 1, 2, 3, 4, 5]));
 
 
 /**
@@ -237,8 +236,9 @@ console.log(toArrayOfSquares([0, 1, 2, 3, 4, 5]));
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  // reduce((previousValue, currentValue, currentIndex) => { /* ... */ } )
+  return arr.reduce((a, b, c) => [...a, b + (a[c - 1] || 0)], []);
 }
 
 /**
